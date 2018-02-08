1cad7f3fb4e72887d8ce74a37fa06a082ea235d7
Stan Vogels <stanvogels@live.nl>
2018-02-08 11:16:05 +0100

    Worked on the popups

---
+++ Assets/Scenes/GameScene/Scripts/ButtonBehavours.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehavours : MonoBehaviour {
+
+    public GameObject MoneyPopup;
+
+    public void OnClickAddMoney()
+    {
+        CloseAllWindows();
+        MoneyPopup.SetActive(true);
+        
+    }
+
+
+
+    public void CloseAllWindows()
+    {
+
+    }
+}
